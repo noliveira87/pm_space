aa0316a39cf84d87bd592e41d5878f7b00e885ba
NUNOLIV <nuno.oliveira@mercedez-benz.io>
2024-06-28 11:41:31 +0100

    Add button New team member

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import ProjectTable from './components/Projects/ProjectTable';
 import AddProject from './components/Projects/AddProject';
 import EditProject from './components/Projects/EditProject';
 import TeamTable from './components/TeamMembers/TeamTable';
 import AddTeamMember from './components/TeamMembers/AddTeamMember';
 import EditTeamMember from './components/TeamMembers/EditTeamMember';
+
+const AddMemberButton = () => {
+  return (
+    <Link to="/add-team-member">
+      <button>Add New Member</button>
+    </Link>
+  );
+};
 
 const App = () => {
   return (
@@ -17,6 +25,7 @@ const App = () => {
             <ProjectTable />
             <h2>Team Members</h2>
             <TeamTable />
+            <AddMemberButton />
           </Route>
           <Route path="/add-project" component={AddProject} />
           <Route path="/edit-project/:id" component={EditProject} />
